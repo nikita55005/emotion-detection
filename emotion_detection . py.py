69cbb461d766d840463e21c4e3c213180e7d77f4
nikita55005 <doranikita4@gmail.com>
2026-04-08 23:12:02 +0530

    Added emotion detection code

---
+++ emotion_detection . py.py
@@ -0,0 +1,65 @@
+#relational operators 
+a = 50 
+b = 20
+
+print (a == b ) #Flase
+print (a != b) #True 
+print (a >= b) #True
+print (a > b ) #Flase
+  
+  #assigment operator 
+num = 10
+num = num + 10
+num += 10
+print("num :" , num )
+# l0gical operators
+print (not False)
+print (not True )
+a = 50
+b = 30
+print (not False)
+print (not (a > b))
+
+Val1 = True 
+Val2 = False
+print ("AND operator:" , Val1 and Val2)
+
+print ("OR operator :", Val1 or Val2 )
+Val1 = False
+Val2 = False
+print ("AND operator",  Val1 and Val2 )
+
+print ("OR operator" , Val1 or Val2 )
+
+print ("hello world")
+#type conversion 
+a = 2 
+b = 4.25
+sum = a  + b # 2.0 + 4.25 = 6.25
+print(sum)
+#type conversion 
+a = int("2")
+b = 4.25
+print (type(a))
+print ( a + b)
+a = 3.14
+b = str (a)
+print (type (a))
+input("enter your name:")
+
+name = input ("enter your name: ")
+print ("welcome " , name ) 
+name = input ("enter your age:")
+print("your entered" , ) 
+int ("5")
+VAl = int (input("enter some value:"))
+print(type(VAl), VAl )
+name = input("enter name:")
+age = input("enter your age:")
+marks = input("enter marks:")
+
+print("welcome" ,name )
+print("age =" , age) 
+
+
+
